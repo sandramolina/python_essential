1a0e93b16c05a8a4247524d3562e2cf1b80f47b1
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-03-19 12:37:38 +0000

    updates exc

--- Exercise Files/01/typecheck_start.py
+++ Exercise Files/01/typecheck_start.py
@@ -18,10 +18,16 @@ b2 = Book("The Grapes of Wrath")
 n1 = Newspaper("The Washington Post")
 n2 = Newspaper("The New York Times")
 
-# TODO: use type() to inspect the object type
-
+# TODO: use type() to inspect the object type 
+print(type(b1))
+print(type(n1))
 
 # TODO: compare two types together
-
+print(type(b1) == type(b2))
+print(type(b1) == type(n1))
 
 # TODO: use isinstance to compare a specific instance to a known type
+print(isinstance(b1, Book))
+print(isinstance(n1, Newspaper))
+print(isinstance(b1, Newspaper))
+print(isinstance(n2, object)) #all classes in python derive from object
